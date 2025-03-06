c1661ae40d41580bc55b5c8d820f530ad7749812
RM <marufrasul24@gmail.com>
2025-03-06 17:20:00 +0500

    feat Frontend javascriptni qurish,Update + MITASK-D

--- app.js
+++ app.js
@@ -1,73 +1,102 @@
-console.log('Web Serverni boshlash');
+console.log('Web servarni boshlash');
 const express = require('express');
-const mongodb = require('mongodb');
 const app = express();
+const fs = require('fs');
 
+//Mongo db chaqiish
 const db = require('./server').db();
+const mongodb = require('mongodb');
 
+// let user;
+// fs.readFile("database/user.json", "utf8", (err, data) => {
+//   if (err) {
+//     console.log("ERROR", err);
+//   } else {
+//     user = JSON.parse(data);
+//   }
+// });
+
+// MongoDB connect
+
+// 1 Kirish code
 app.use(express.static('public'));
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// 2 Session code
+// 3Views code
 app.set('views', 'views');
 app.set('view engine', 'ejs');
 
-app.post('/create-item', (req, res) => {
-  console.log('user entered /create-item');
-  const newReja = req.body.reja;
+// 4 Routing code
 
-  db.collection('plans').insertOne({ reja: newReja }, (err, result) => {
+app.post('/create-item', function (req, res) {
+  console.log('User entered /create-item');
+
+  const new_reja = req.body.reja;
+  if (!new_reja) {
+    return res.status(400).json({ error: 'Reja kiritilishi shart!' });
+  }
+
+  db.collection('plans').insertOne({ reja: new_reja }, function (err, data) {
     if (err) {
-      console.error(err);
-      return res.status(500).send('Something went wrong while inserting data.');
+      console.error('Xatolik yuz berdi:', err);
+      return res.status(500).json({ error: 'Ichki server xatosi' });
     }
 
-    if (result.ops) {
-      console.log('Inserted doc:', result.ops[0]);
-      return res.json(result.ops[0]);
-    }
-
-    const insertedDoc = {
-      _id: result.insertedId,
-      reja: newReja,
-    };
-    console.log('Inserted doc:', insertedDoc);
-    return res.json(insertedDoc);
+    console.log(data);
+    res.json({ _id: data.insertedId, reja: new_reja });
   });
 });
 
 app.post('/delete-item', (req, res) => {
   const id = req.body.id;
-  console.log('Deleting item with id:', id);
 
   db.collection('plans').deleteOne(
     { _id: new mongodb.ObjectId(id) },
-    (err, data) => {
-      if (err) {
-        console.error(err);
-        return res.status(500).send('Error while deleting');
-      }
-      console.log('Document deleted count:', data.deletedCount);
+    function (err, data) {
       res.json({ state: 'success' });
     }
   );
 });
 
-app.get('/', (req, res) => {
+app.post('/edit-item', (req, res) => {
+  const data = req.body;
+  console.log(data);
+  db.collection('plans').findOneAndUpdate(
+    { _id: new mongodb.ObjectId(data.id) },
+    { $set: { reja: data.new_input } },
+    function (err, data) {
+      res.json({ state: 'success' });
+    }
+  );
+});
+
+app.post('/delete-all', (req, res) => {
+  console.log('nimadur', req.body.delete_all);
+  if (req.body.delete_all) {
+    console.log('nimadur', req.body.delete_all);
+    db.collection('plans').deleteMany({}, function (err, result) {
+      if (err) {
+        return res.json({ state: "Hatolik bo'ldi" });
+      }
+
+      res.json({ state: 'hamma rejalar ochirildi' });
+    });
+  }
+});
+
+app.get('/', function (req, res) {
   console.log('user entered /');
   db.collection('plans')
     .find()
     .toArray((err, data) => {
       if (err) {
-        console.error(err);
-        return res
-          .status(500)
-          .send('Something went wrong while fetching data.');
+        console.log(err);
+        res.end('something went wromg');
+      } else {
+        res.render('reja', { items: data });
       }
-      console.log('Data from DB:', data);
-
-      res.render('reja', { items: data });
     });
 });
 
